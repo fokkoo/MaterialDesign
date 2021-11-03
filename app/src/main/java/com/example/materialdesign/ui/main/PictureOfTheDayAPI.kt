c012be3c6579c323f78b0ed3b2746f13b9da1e70
KazanskiiPN <fokkoo@yandex.ru>
2021-11-03 22:56:38 +0300

    0.55 PictureOfTheDayAPI is added

--- app/src/main/java/com/example/materialdesign/ui/main/PictureOfTheDayAPI.kt
+++ app/src/main/java/com/example/materialdesign/ui/main/PictureOfTheDayAPI.kt
@@ -1,11 +1,12 @@
 package com.example.materialdesign.ui.main
 
-import android.telecom.Call
+
+import retrofit2.Call
 import retrofit2.http.GET
 import retrofit2.http.Query
 
 interface PictureOfTheDayAPI {
-  //  @GET("planetary/apod")
-   // fun getPictureOfTheDay(@Query("api_key") apiKey: String): Call<PODServerResponseData>
+    @GET("planetary/apod")
+    fun getPictureOfTheDay(@Query("api_key") apiKey: String): Call<PODServerResponseData>
 
 }
