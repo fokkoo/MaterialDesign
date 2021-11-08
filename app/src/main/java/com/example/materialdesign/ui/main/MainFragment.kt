0f75f060acd1c7bb8dab86a615e92952aa3959a2
KazanskiiPN <fokkoo@yandex.ru>
2021-11-08 21:29:52 +0300

    0.55 PictureOfTheDayAPI is added search for my bug

--- app/src/main/java/com/example/materialdesign/ui/main/MainFragment.kt
+++ app/src/main/java/com/example/materialdesign/ui/main/MainFragment.kt
@@ -25,8 +25,7 @@ class MainFragment : Fragment() {
 
     }
 
-//
-//
+
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
         viewModel = ViewModelProvider(this).get(MainViewModel::class.java)
